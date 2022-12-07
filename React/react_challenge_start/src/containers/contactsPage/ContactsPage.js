059ab931e43c1428db25f2136894a2b243ebad90
chrosslaw <Chrosslaw@gmail.com>
2022-12-07 11:56:13 -0600

    First commit

---
+++ React/react_challenge_start/src/containers/contactsPage/ContactsPage.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { ContactForm } from "../../components/contactForm/ContactForm";
+import { TileList } from "../../components/tileList/TileList";
+
+export const ContactsPage = (props) => {
+  /*
+  Define state variables for 
+  contact info and duplicate check
+  */
+  const [contacts, setContacts] = useState(props);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [duplicate, setDuplicate] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*
+    Add contact info and clear data
+    if the contact name is not a duplicate
+    */
+    const newName = e.target.value;
+    if (!duplicate) {
+    }
+  };
+
+  /*
+  Using hooks, check for contact name in the 
+  contacts array variable in props
+  */
+
+  return (
+    <div>
+      <section>
+        <h2>Add Contact</h2>
+        <ContactForm name={name} phone={phone} email={email} />
+      </section>
+      <hr />
+      <section>
+        <h2>Contacts</h2>
+        <TileList />
+      </section>
+    </div>
+  );
+};
