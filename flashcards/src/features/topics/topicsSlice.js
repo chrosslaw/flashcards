b41c9e09f96f2bca123afb82ca4d4ff50d61ade4
chrosslaw <Chrosslaw@gmail.com>
2023-01-09 11:35:08 -0600

    create addTopic action

--- flashcards/src/features/topics/topicsSlice.js
+++ flashcards/src/features/topics/topicsSlice.js
@@ -1,8 +1,18 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 export const topicsSlice = createSlice({
   name: "topics",
   initialState: {
     topics: {}
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      const { id, name, icon } = action.payload;
+      state.topics[id] = {
+        id: id,
+        name: name,
+        icon: icon
+      };
+    }
   }
 });
